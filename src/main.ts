107bccefbfad1117bf456a0433d99cbcd6c2f755
Prashanth Naika <pnaika@egen.solutions>
2020-03-22 12:06:07 -0700

    code merge

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from "@nestjs/common";
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+
+  if (process.env.NODE_ENV === 'development') {
+    logger.log(`Application Started in DEV mode`);
+    app.enableCors();
+  }
+
+  const serverConfig = process.env.port || config.get('server');
+  const port = serverConfig.port;
 
   await app.listen(port);
   logger.log(`Application Started at port ${port}`)
